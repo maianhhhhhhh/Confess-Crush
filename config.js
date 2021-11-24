250cbd7b83bf1bf2b7351766a49125ef167db5b0
maianhhhhhhh <94727888+maianhhhhhhh@users.noreply.github.com>
2021-11-24 21:53:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh iu',
     reply: 'iu iu cái ',
     mess: 'Bố m biết mà. Yêu m nhiều nhiều vl',
-    messDesc: 'sắp sinh nhật t nhớ mua bim ',
+    messDesc: 'sắp sinh nhật t nhớ mua bimbimm',
     btnAccept: 'ừ :)?',
-    messLink: 'https://github.com/qqlammaidellxong/iulammoidualink' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://github.com/qqlammaidellxong/iulammoidualinkdo' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
